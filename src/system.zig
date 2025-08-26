aa6a3d6d10ee7082a33058a956bf1b8867ed45d5
evaleek <dev@evii.li>
2025-08-26 19:06:06 -0400

    Define CMSG ancillary data

--- src/system.zig
+++ src/system.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
+const mem = std.mem;
 const posix = std.posix;
 const system = posix.system;
 const assert = std.debug.assert;
 
-pub fn connect(path: []const u8) !posix.socket_t {
+const Socket = posix.socket_t;
+const FD = posix.fd_t;
+
+pub fn connect(path: []const u8) !Socket {
     const SocketAddress = system.sockaddr.un;
     const path_buffer_len = @typeInfo(@FieldType(SocketAddress, "path")).array.len;
     var addr = SocketAddress{
@@ -28,7 +32,7 @@ pub fn connect(path: []const u8) !posix.socket_t {
     const size: system.socklen_t = @offsetOf(SocketAddress, "path")
         + @as(system.socklen_t, @intCast(path_len_with_sentinel));
 
-    const fd: posix.socket_t = try posix.socket(
+    const fd: Socket = try posix.socket(
         system.PF.LOCAL,
         system.SOCK.STREAM | system.SOCK.CLOEXEC | system.SOCK.NONBLOCK,
         0,
@@ -39,3 +43,43 @@ pub fn connect(path: []const u8) !posix.socket_t {
 
     return fd;
 }
+
+pub const cmsg = struct {
+    pub const hdr = extern struct {
+        cmsg_len: system.socklen_t,
+        cmsg_level: c_int,
+        cmsg_type: c_int,
+        // __extension__ unsigned char __cmsg_data __flexarr // Ancillary data.
+    };
+
+    // #define CMSG_DATA(cmsg) ((unsigned char *) (cmsg) + CMSG_ALIGN (sizeof (struct cmsghdr)))
+    pub fn data(mhdr: *hdr) *u8 {
+        return @as(*u8, @ptrCast(mhdr)) + @"align"(@sizeOf(hdr));
+    }
+
+    // #define CMSG_FIRSTHDR(mhdr)
+    // ((size_t) (mhdr)->msg_controllen >= sizeof (struct cmsghdr)
+    //     ? (struct cmsghdr *) (mhdr)->msg_control
+    //     : (struct cmsghdr *) 0)
+    pub fn firsthdr(mhdr: posix.msghdr) ?*hdr {
+        return if ( mhdr.msg_controllen >= @sizeOf(hdr) )
+            @ptrCast(mhdr.msg_control) else null;
+    }
+
+    // #define CMSG_ALIGN(len) (((len) + sizeof (size_t) - 1) & (size_t) ~(sizeof (size_t) - 1))
+    pub fn @"align"(length: usize) usize {
+        const size_t_size: usize = @sizeOf(usize);
+        const rem_bits: usize = size_t_size - 1;
+        return ( length + rem_bits ) & ( ~rem_bits );
+    }
+
+    // #define CMSG_SPACE(len) (CMSG_ALIGN (len) + CMSG_ALIGN (sizeof (struct cmsghdr)))
+    pub fn space(length: usize) usize {
+        return @"align"(length) + @"align"(@sizeOf(hdr));
+    }
+
+    // #define CMSG_LEN(len) (CMSG_ALIGN (sizeof (struct cmsghdr)) + (len))
+    pub fn len(length: usize) usize {
+        return @"align"(@sizeOf(hdr)) + length;
+    }
+};
